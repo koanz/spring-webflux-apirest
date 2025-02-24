d9c97594f30ea6ffd3893cef1075319b1b464cf8
Cristian <cristiananzawa@gmail.com>
2025-02-24 10:30:57 -0300

    message source and router function config

---
+++ src/main/java/com/idea/springboot/webflux/app/config/RouterFunctionConfig.java
@@ -0,0 +1,15 @@
+package com.idea.springboot.webflux.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class RouterFunctionConfig {
+
+//    @Bean
+//    public RouterFunction<ServerResponse> routes() {
+//
+//    }
+}
