6603a07f874693386ccf81904fe9110ac667e1a2
Cristian <cristiananzawa@gmail.com>
2025-02-24 10:30:23 -0300

    update message response in create, update and delete operations

--- src/main/java/com/idea/springboot/webflux/app/controllers/CategoryRestController.java
+++ src/main/java/com/idea/springboot/webflux/app/controllers/CategoryRestController.java
@@ -4,9 +4,12 @@ import com.idea.springboot.webflux.app.models.MessageResponse;
 import com.idea.springboot.webflux.app.models.dtos.CategoryDTO;
 import com.idea.springboot.webflux.app.services.CategoryService;
 import com.idea.springboot.webflux.app.validations.OnCreate;
+import com.idea.springboot.webflux.app.validations.OnProductUpdate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +17,18 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Date;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api/v1/categories")
 public class CategoryRestController {
     private static final Logger logger = LoggerFactory.getLogger(ProductRestController.class);
+
+    @Value("${category.field.name.message}")
+    private String FIELD_NAME;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Autowired
     private CategoryService categoryService;
@@ -38,8 +48,20 @@ public class CategoryRestController {
         Date currentDate = new Date();
 
         return categoryService.save(request).map(responseDto -> {
-            MessageResponse response = new MessageResponse("Successfully created", currentDate.toString());
-            response.addDynamicFieldName("category", responseDto);
+            MessageResponse response = new MessageResponse(messageSource.getMessage("creation.message", null, Locale.getDefault()), currentDate.toString());
+            response.addDynamicFieldName(FIELD_NAME, responseDto);
+
+            return ResponseEntity.ok(response);
+        });
+    }
+
+    @PutMapping("/update/{id}")
+    public Mono<ResponseEntity<MessageResponse>> update(@PathVariable String id, @Validated(OnProductUpdate.class) @RequestBody CategoryDTO request) {
+        Date currentDate = new Date();
+
+        return categoryService.update(id, request).map(responseDto -> {
+            MessageResponse response = new MessageResponse(messageSource.getMessage("update.message", null, Locale.getDefault()), currentDate.toString());
+            response.addDynamicFieldName(FIELD_NAME, responseDto);
 
             return ResponseEntity.ok(response);
         });
@@ -50,6 +72,6 @@ public class CategoryRestController {
         Date currentDate = new Date();
 
         return categoryService.delete(id)
-                .then(Mono.just(ResponseEntity.ok(new MessageResponse("Successfully deleted", currentDate.toString()))));
+                .then(Mono.just(ResponseEntity.ok(new MessageResponse(messageSource.getMessage("update.message", null, Locale.getDefault()), currentDate.toString()))));
     }
 }
--- src/main/java/com/idea/springboot/webflux/app/controllers/ProductRestController.java
+++ src/main/java/com/idea/springboot/webflux/app/controllers/ProductRestController.java
@@ -8,6 +8,8 @@ import com.idea.springboot.webflux.app.validations.OnProductUpdate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +17,18 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Date;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductRestController {
     private static final Logger logger = LoggerFactory.getLogger(ProductRestController.class);
+
+    @Value("${product.field.name.message}")
+    private String FIELD_NAME;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Autowired
     private ProductService productService;
@@ -39,8 +48,8 @@ public class ProductRestController {
         Date currentDate = new Date();
 
         return productService.save(request).map(responseDto -> {
-            MessageResponse response = new MessageResponse("Successfully created", currentDate.toString());
-            response.addDynamicFieldName("product", responseDto);
+            MessageResponse response = new MessageResponse(messageSource.getMessage("creation.message", null, Locale.getDefault()), currentDate.toString());
+            response.addDynamicFieldName(FIELD_NAME, responseDto);
 
             return ResponseEntity.ok(response);
         });
@@ -51,8 +60,8 @@ public class ProductRestController {
         Date currentDate = new Date();
 
         return productService.update(id, request).map(productDTO -> {
-            MessageResponse response = new MessageResponse("Successfully created", currentDate.toString());
-            response.addDynamicFieldName("product", productDTO);
+            MessageResponse response = new MessageResponse(messageSource.getMessage("update.message", null, Locale.getDefault()), currentDate.toString());
+            response.addDynamicFieldName(FIELD_NAME, productDTO);
 
             return ResponseEntity.ok(response);
         });
@@ -63,7 +72,7 @@ public class ProductRestController {
         Date currentDate = new Date();
 
         return productService.delete(id)
-                .then(Mono.just(ResponseEntity.ok(new MessageResponse("Successfully deleted", currentDate.toString()))));
+                .then(Mono.just(ResponseEntity.ok(new MessageResponse(messageSource.getMessage("delete.message", null, Locale.getDefault()), currentDate.toString()))));
     }
 
 }
