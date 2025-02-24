aaa7c0d9f32faaf819d34ccc3e8e78696389ff49
Cristian <cristiananzawa@gmail.com>
2025-02-24 17:59:06 -0300

    missing ''

--- src/main/java/com/idea/springboot/webflux/app/exceptions/ProductNotFoundException.java
+++ src/main/java/com/idea/springboot/webflux/app/exceptions/ProductNotFoundException.java
@@ -2,6 +2,6 @@ package com.idea.springboot.webflux.app.exceptions;
 
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(String id) {
-        super("Product with id " + id + " not found");
+        super("Product with id '" + id + "' not found");
     }
 }
