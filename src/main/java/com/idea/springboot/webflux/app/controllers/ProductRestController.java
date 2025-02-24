a2bd7d292113f38e1ded61e0b1d7272d635978dc
Cristian <cristiananzawa@gmail.com>
2025-02-24 09:53:39 -0300

    removing error responses

--- src/main/java/com/idea/springboot/webflux/app/controllers/ProductRestController.java
+++ src/main/java/com/idea/springboot/webflux/app/controllers/ProductRestController.java
@@ -3,12 +3,11 @@ package com.idea.springboot.webflux.app.controllers;
 import com.idea.springboot.webflux.app.models.MessageResponse;
 import com.idea.springboot.webflux.app.models.dtos.ProductDTO;
 import com.idea.springboot.webflux.app.services.ProductService;
-import com.idea.springboot.webflux.app.validations.OnCreate;
-import com.idea.springboot.webflux.app.validations.OnUpdate;
+import com.idea.springboot.webflux.app.validations.OnProductCreate;
+import com.idea.springboot.webflux.app.validations.OnProductUpdate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -36,37 +35,35 @@ public class ProductRestController {
     }
 
     @PostMapping("/create")
-    public Mono<ResponseEntity<MessageResponse>> create(@Validated(OnCreate.class) @RequestBody ProductDTO request) {
+    public Mono<ResponseEntity<MessageResponse>> save(@RequestBody @Validated(OnProductCreate.class) ProductDTO request) {
         Date currentDate = new Date();
-        return productService.save(request)
-                .map(productDTO -> {
-                    return ResponseEntity.ok(new MessageResponse("Successfully created", currentDate.toString(), productDTO));
-                })
-                .onErrorResume(error -> {
-                    logger.error("Error while trying to create the Product.", error);
-                    return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                            .body(new MessageResponse("Error while trying to create the Product.", currentDate.toString(), null)));
-                });
+
+        return productService.save(request).map(responseDto -> {
+            MessageResponse response = new MessageResponse("Successfully created", currentDate.toString());
+            response.addDynamicField("product", responseDto);
+
+            return ResponseEntity.ok(response);
+        });
     }
 
     @PutMapping("/update/{id}")
-    public Mono<ResponseEntity<MessageResponse>> update(@PathVariable String id, @Validated(OnUpdate.class) @RequestBody ProductDTO request) {
+    public Mono<ResponseEntity<MessageResponse>> update(@PathVariable String id, @Validated(OnProductUpdate.class) @RequestBody ProductDTO request) {
         Date currentDate = new Date();
+
         return productService.update(id, request).map(productDTO -> {
-                    return ResponseEntity.ok(new MessageResponse("Successfully updated", currentDate.toString(), productDTO));
-                })
-                .onErrorResume(error -> {
-                    logger.error("Error while trying to update the Product.", error);
-                    return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                            .body(new MessageResponse("Error while trying to updated the Product.", currentDate.toString(), null)));
-                });
+            MessageResponse response = new MessageResponse("Successfully created", currentDate.toString());
+            response.addDynamicField("product", productDTO);
+
+            return ResponseEntity.ok(response);
+        });
     }
 
     @DeleteMapping("/delete/{id}")
-    public Mono<ResponseEntity<String>> delete(@PathVariable String id) {
+    public Mono<ResponseEntity<MessageResponse>> delete(@PathVariable String id) {
+        Date currentDate = new Date();
+
         return productService.delete(id)
-                .then(Mono.just(ResponseEntity.ok("Successfully deleted.")))
-                .doOnError(error -> logger.error("Error while trying to delete the Product.", error));
+                .then(Mono.just(ResponseEntity.ok(new MessageResponse("Successfully deleted", currentDate.toString()))));
     }
 
 }
