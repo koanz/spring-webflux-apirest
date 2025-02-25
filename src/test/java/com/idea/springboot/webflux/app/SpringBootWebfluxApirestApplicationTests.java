95d01949bbc2c6ce2435abca551b02f9f6497a0c
Cristian <cristiananzawa@gmail.com>
2025-02-25 18:50:18 -0300

    first integration test of product

--- src/test/java/com/idea/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/idea/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.idea.springboot.webflux.app;
 
+import com.idea.springboot.webflux.app.models.dtos.ProductDTO;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	public void listTest() {
+		client.get()
+				.uri("/api/v1/products/")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(ProductDTO.class)
+				.hasSize(16);
 	}
 
 }
