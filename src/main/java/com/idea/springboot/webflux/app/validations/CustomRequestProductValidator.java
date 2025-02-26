580e75ebc3a7dd2f575c7df5a3ec79e748630846
Cristian <cristiananzawa@gmail.com>
2025-02-26 20:01:07 -0300

    renaming

--- src/main/java/com/idea/springboot/webflux/app/validations/CustomRequestProductValidator.java
+++ src/main/java/com/idea/springboot/webflux/app/validations/CustomRequestProductValidator.java
@@ -5,7 +5,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-public class CustomRequestValidator implements Validator {
+public class CustomRequestProductValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> clazz) {
