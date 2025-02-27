2008aa598972bdbd036ecf291258ad4c2c5a40e4
Cristian <cristiananzawa@gmail.com>
2025-02-27 15:13:13 -0300

    exception added

--- src/main/java/com/idea/springboot/webflux/app/exceptions/CategoryNotFountByIdException.java
+++ src/main/java/com/idea/springboot/webflux/app/exceptions/CategoryNotFountByIdException.java
@@ -1,7 +1,7 @@
 package com.idea.springboot.webflux.app.exceptions;
 
-public class CategoryNotFountException extends RuntimeException {
-    public CategoryNotFountException(String id) {
+public class CategoryNotFountByIdException extends RuntimeException {
+    public CategoryNotFountByIdException(String id) {
         super("Category with id '" + id + "' not found");
     }
 }
