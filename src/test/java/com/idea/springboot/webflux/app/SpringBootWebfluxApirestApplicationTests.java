431c7bce33be1904ce71c4deff123667cc12335b
Cristian <cristiananzawa@gmail.com>
2025-02-27 17:53:48 -0300

    environment annotation for integration test updated

--- src/test/java/com/idea/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/idea/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -19,8 +20,9 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
 
+@AutoConfigureWebTestClient
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class SpringBootWebfluxApirestApplicationTests {
 
 	@Autowired
