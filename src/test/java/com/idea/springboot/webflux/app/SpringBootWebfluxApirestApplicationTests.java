e766dec34c72c333311ca4f2995f0040806a1c7f
Cristian <cristiananzawa@gmail.com>
2025-02-27 15:15:16 -0300

    test: integration test of product

--- src/test/java/com/idea/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/idea/springboot/webflux/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,6 +1,11 @@
 package com.idea.springboot.webflux.app;
 
+import com.idea.springboot.webflux.app.models.MessageResponse;
+import com.idea.springboot.webflux.app.models.dtos.CategoryDTO;
 import com.idea.springboot.webflux.app.models.dtos.ProductDTO;
+
+import com.idea.springboot.webflux.app.services.CategoryService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import java.util.Map;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,16 +25,123 @@ class SpringBootWebfluxApirestApplicationTests {
 	@Autowired
 	private WebTestClient client;
 
+	@Autowired
+	private CategoryService categoryService;
+
 	@Test
 	public void listTest() {
 		client.get()
-				.uri("/api/v1/products/")
+				.uri("/api/v2/products/")
 				.accept(MediaType.APPLICATION_JSON)
 				.exchange()
 				.expectStatus().isOk()
 				.expectHeader().contentType(MediaType.APPLICATION_JSON)
 				.expectBodyList(ProductDTO.class)
 				.hasSize(16);
+	}
+
+	@Test
+	public void notEmptyListWithConsumeWithTest() {
+		client.get()
+				.uri("/api/v2/products/")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(ProductDTO.class)
+				.consumeWith(response -> {
+					Assertions.assertTrue(response.getResponseBody().size() > 0);
+				});
+	}
+
+	@Test
+	public void noRecordExistMessageAndStatusCodeTest() {
+		client.get()
+				.uri("/api/v2/products/")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().is4xxClientError()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.message").isEqualTo("No records exist");
+	}
+
+	@Test
+	public void statusCodeNotFoundByIdTest() {
+		String id = "67bf9ad0d572bc1cde0586a";
+
+		client.get()
+				.uri("/api/v2/products/{id}", id)
+				.accept(MediaType.APPLICATION_JSON)
+				.header("id", id)
+				.exchange()
+				.expectStatus().is4xxClientError()
+				.expectStatus().isNotFound()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.message").isEqualTo("Product with id '"+ id +"' not found")
+				.jsonPath("$.status").isEqualTo(404);
+	}
+
+	@Test
+	public void existProductByIdTest() {
+		String id = "67bf9ad0d572bc1cde0586ab";
+
+		client.get()
+				.uri("/api/v2/products/{id}", id)
+				.accept(MediaType.APPLICATION_JSON)
+				.header("id", id)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody(ProductDTO.class)
+				.consumeWith(response -> {
+					ProductDTO responseDto = response.getResponseBody();
+					Assertions.assertEquals(responseDto.getId(), id);
+					Assertions.assertNotNull(responseDto.getName());
+					Assertions.assertEquals(responseDto.getName(), "Samsung S23 FE 128GB");
+					Assertions.assertNotNull(responseDto.getPrice());
+					Assertions.assertEquals(responseDto.getPrice(), 389.99);
+
+				});
+	}
+
+	@Test
+	public void createTest() {
+		CategoryDTO categoryDTO = categoryService.findByName("Technology").block();
+		ProductDTO productDTO = new ProductDTO();
+		productDTO.setName("Iphone 14 XS 256GB");
+		productDTO.setPrice(1269.99);
+		productDTO.setCategory(categoryDTO);
+
+		client.post()
+				.uri("/api/v2/products/create")
+				.accept(MediaType.APPLICATION_JSON)
+				.body(BodyInserters.fromValue(productDTO))
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody(ProductDTO.class)
+				.consumeWith(response -> {
+					ProductDTO responseDto = response.getResponseBody();
+					// Verifica que la respuesta no sea nula
+					Assertions.assertNotNull(responseDto);
+
+					// Verifica los campos del producto
+					Assertions.assertNotNull(responseDto.getId());
+
+					Assertions.assertEquals(responseDto.getName(), productDTO.getName());
+					Assertions.assertEquals(responseDto.getPrice(), productDTO.getPrice());
+
+					// Verifica los campos de categoría y created_at
+					Assertions.assertNotNull(responseDto.getCategory());
+					// Verifica que la categoría sea la misma
+					Assertions.assertEquals(responseDto.getCategory().getId(), categoryDTO.getId());
+					Assertions.assertEquals(responseDto.getCategory().getName(), categoryDTO.getName());
+					Assertions.assertEquals(responseDto.getCategory().getCreatedAt(), categoryDTO.getCreatedAt());
+
+					Assertions.assertNotNull(responseDto.getCreatedAt());
+				});
 	}
 
 }
